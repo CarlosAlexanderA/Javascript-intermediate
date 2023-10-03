a6f34142b1e0fb1914100eee6d128bd3d48baa67
CarlosAlexanderA <acerohuarahuara7551@gmail.com>
2023-10-02 21:29:59 -0500

    page restauran con webpack y con tabs que no actualizan la pagina, part1(solo config de webpack y page con html)

---
+++ page-restaurant/src/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+// import hamburguer1 from "./images/hamburguer-1.jpg";
+// function component(){
+//     const element = document.createElement("div");
+//     element.innerText = "hello webpack"
+
+//     return element;
+
+// }
+// const content = document.getElementById("content");
+// content.appendChild(component());
