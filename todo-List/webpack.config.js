907c0d8fbf550b05f495112df559f4c340e1f2cb
CarlosAlexanderA <acerohuarahuara7551@gmail.com>
2023-10-04 21:44:02 -0500

    lista de tareas(todo-list), parte frontend part-1

---
+++ todo-List/webpack.config.js
@@ -0,0 +1,32 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("node:path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "build"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "src/index.html" })],
+  devServer: {
+    open: true, //abrir el navergador
+    port: 3100, // puerto de carga
+  },
+};
