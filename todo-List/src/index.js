e74375de974cd77ed5acdb98866cc71e5d502193
CarlosAlexanderA <acerohuarahuara7551@gmail.com>
2023-10-10 00:47:39 -0500

    guardando con el form la lista de to-do

--- todo-List/src/index.js
+++ todo-List/src/index.js
@@ -1,21 +1,31 @@
 import "./style.css";
-
-const addToDo = document.getElementById("add-to-do");
+const { format, parse } = require("date-fns");
+const btnAdd = document.getElementById("btn-add");
 const modalFrame = document.getElementById("modal-add");
 const closeModal = document.getElementById("close-modal");
-addToDo.addEventListener("click", () => {
+btnAdd.addEventListener("click", () => {
   modalFrame.style.display = "flex";
 });
-
-closeModal.addEventListener("click", () => {
+const closeadd = () => {
   modalFrame.style.display = "none";
-});
+};
+closeModal.addEventListener("click", closeadd);
 let idNum = 0;
 class ToDo {
   constructor(title, descripton, date, priority, project) {
     this.title = title;
     this.descripton = descripton;
     this.date = date;
+    const inputDate = date;
+
+    const parsedDate = parse(inputDate, "yyyy-MM-dd", new Date());
+
+    const formattedDate = format(parsedDate, "MMM");
+
+    const dayOfMonth = format(parsedDate, "do");
+
+    this.date = `${formattedDate} ${dayOfMonth}`;
+
     this.priority = priority;
     this.project = project;
     this.idCard = idNum++;
@@ -42,7 +52,6 @@ class ToDo {
     cont.appendChild(checkInput);
     left.append(cont, titleNote);
     card.append(left, right);
-
     return card;
   }
   check() {
@@ -85,6 +94,7 @@ class ToDo {
     });
     return editBtn;
   }
+  modal() {}
 }
 const options = document.querySelectorAll("main aside nav ul li a");
 // console.log(options);
@@ -134,15 +144,15 @@ btnChecked().forEach((btn) => {
     }
   });
 });
-const play = new ToDo("play", "play with friends", "24-04-06", "low", "gym");
-const example1 = new ToDo("example1", "example1", "24-04-06", "low", "week");
-const example2 = new ToDo("example2", "example2", "24-04-06", "low", "week");
-const example3 = new ToDo("example3", "example3", "24-04-06", "low", "today");
-const example4 = new ToDo("example4", "example4", "24-04-06", "low", "gym");
-const example5 = new ToDo("example5", "example5", "24-04-06", "low", "today");
-const example6 = new ToDo("example6", "example6", "24-04-06", "low", "today");
-const example7 = new ToDo("example7", "example7", "24-04-06", "low", "gym");
-const example8 = new ToDo("example8", "example8", "24-04-06", "low", "gym");
+const play = new ToDo("play", "play with friends", "2022-11-06", "low", "gym");
+const example1 = new ToDo("example1", "example1", "2022-11-06", "low", "week");
+const example2 = new ToDo("example2", "example2", "2022-11-06", "low", "week");
+const example3 = new ToDo("example3", "example3", "2022-11-06", "low", "today");
+const example4 = new ToDo("example4", "example4", "2022-11-06", "low", "gym");
+const example5 = new ToDo("example5", "example5", "2022-11-06", "low", "today");
+const example6 = new ToDo("example6", "example6", "2022-11-06", "low", "today");
+const example7 = new ToDo("example7", "example7", "2022-11-06", "low", "gym");
+const example8 = new ToDo("example8", "example8", "2022-11-06", "low", "gym");
 
 // console.log(play.build());
 // console.log(play.project);
@@ -159,4 +169,22 @@ listNotes.append(
   example8.build()
 );
 
-console.log(elementById(0));
+// console.log(elementById(0));
+
+const addToDo = document.getElementById("add-to-do");
+
+addToDo.addEventListener("submit", () => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+
+  const title = formData.get("title");
+  const detail = formData.get("detail");
+  const datetime = formData.get("datetime");
+  const priority = formData.get("priority");
+  const project = "gym";
+  console.log(datetime);
+  // console.log(formData);
+  const card = new ToDo(title, detail, datetime, priority, project);
+  listNotes.appendChild(card.build());
+  closeadd();
+});
